9d2166587c96f4fdf206fd34d9c6cb6cf86a3e7f
Farendivaz <dandyfariz9@gmail.com>
2023-07-17 19:24:47 +0700

    add html tag for google search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import "./globals.css";
 import { Providers } from "./Providers";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const inter = Inter({
   weight: ["400", "700"],
@@ -14,27 +15,39 @@ const inter = Inter({
 export const metadata = {
   title: "Farendivaz",
   description: "Farendivaz Personal Website",
+  // google-site-verification: ""
 };
-
+{
+}
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="w-full h-full mx-auto bg-cover bg-zinc-100 text-black/90 dark:bg-zinc-900 dark:text-white/90">
-        <Providers>
-          <div className="main">
-            <div className="gradient"></div>
-          </div>
-          <main className="app">
-            <Navbar />
-            {children}
-            <Analytics />
-          </main>
-        </Providers>
-      </body>
-    </html>
+    <>
+      <Head>
+        <title>Farendivaz</title>
+        <meta name="description">Farendivaz Personal Website</meta>
+        <meta
+          name="google-site-verification"
+          content="sAW_62gARtx7mQWt0mKm4b4PGBZRcSQ4O1_iNzBrYYo"
+        />
+      </Head>
+      <html lang="en" className={inter.className} suppressHydrationWarning>
+        <body className="w-full h-full mx-auto bg-cover bg-zinc-100 text-black/90 dark:bg-zinc-900 dark:text-white/90">
+          <Providers>
+            <div className="main">
+              <div className="gradient"></div>
+            </div>
+            <main className="app">
+              <Navbar />
+              {children}
+              <Analytics />
+            </main>
+          </Providers>
+        </body>
+      </html>
+    </>
   );
 }
