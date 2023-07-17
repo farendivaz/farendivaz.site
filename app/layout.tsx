ca93e08f7dc75addd5d87544ca8a2cb8ddba8cfa
Farendivaz <dandyfariz9@gmail.com>
2023-07-17 20:02:06 +0700

    Delete google search console html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import "./globals.css";
 import { Providers } from "./Providers";
 import { Analytics } from "@vercel/analytics/react";
-import Head from "next/head";
 
 const inter = Inter({
   weight: ["400", "700"],
@@ -12,11 +11,10 @@ const inter = Inter({
   display: "swap",
 });
 
-// export const metadata = {
-//   title: "Farendivaz",
-//   description: "Farendivaz Personal Website",
-//   // google-site-verification: ""
-// };
+export const metadata = {
+  title: "Farendivaz",
+  description: "Farendivaz Personal Website",
+};
 
 export default function RootLayout({
   children,
@@ -24,29 +22,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Head>
-        <title>Farendivaz</title>
-        <meta name="description">Farendivaz Personal Website</meta>
-        <meta
-          name="google-site-verification"
-          content="sAW_62gARtx7mQWt0mKm4b4PGBZRcSQ4O1_iNzBrYYo"
-        />
-      </Head>
-      <html lang="en" className={inter.className} suppressHydrationWarning>
-        <body className="w-full h-full mx-auto bg-cover bg-zinc-100 text-black/90 dark:bg-zinc-900 dark:text-white/90">
-          <Providers>
-            <div className="main">
-              <div className="gradient"></div>
-            </div>
-            <main className="app">
-              <Navbar />
-              {children}
-              <Analytics />
-            </main>
-          </Providers>
-        </body>
-      </html>
-    </>
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body className="w-full h-full mx-auto bg-cover bg-zinc-100 text-black/90 dark:bg-zinc-900 dark:text-white/90">
+        <Providers>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Navbar />
+            {children}
+            <Analytics />
+          </main>
+        </Providers>
+      </body>
+    </html>
   );
 }
